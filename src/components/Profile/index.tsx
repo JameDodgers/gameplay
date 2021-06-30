49d763903cc86416377229e3508a3e2e47d2b064
Pablo Henrique <pablohenrique.r.c@gmail.com>
2021-06-30 16:32:56 -0300

    Implement Sign In functionality
    Handles navigation, changing to home screen if sign in was successful and shows the user's retrieved avatar and first name.

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { useAuth } from "../../hooks/auth";
 
 import { Avatar } from "../Avatar";
 
 import { styles } from "./styles";
 
 export function Profile() {
+  const { user } = useAuth();
+
   return (
     <View style={styles.container}>
-      <Avatar urlImage="https://github.com/jamedodgers.png" />
+      <Avatar urlImage={user.avatar} />
       <View>
         <View style={styles.container}>
           <Text style={styles.greeting}>
-              Olá,
+            Olá,
           </Text>
           <Text style={styles.username}>
-              Pablo
+            {user.firstName}
           </Text>
         </View>
         <Text style={styles.message}>
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
-import { View, Text, Image } from "react-native";
 
-import { useNavigation } from "@react-navigation/native";
+import {
+  View,
+  Text,
+  Image,
+  Alert,
+  ActivityIndicator
+} from "react-native";
+
+import { useAuth } from '../../hooks/auth'
 
 import { ButtonIcon } from "../../components/ButtonIcon";
 
@@ -11,12 +18,20 @@ import { Background } from '../../components/Background';
 
 import { styles } from "./styles";
 
+import { theme } from "../../global/styles/theme";
+
 export function SignIn() {
-  const navigation = useNavigation();
   const [text, setText] = useState("");
 
-  function handleSignIn() {
-    navigation.navigate('Home');
+  const { user, loading, signIn } = useAuth();
+
+  async function handleSignIn() {
+    try {
+      await signIn();
+
+    } catch (error) {
+      Alert.alert(error);
+    }
   }
 
   return (
@@ -37,10 +52,18 @@ export function SignIn() {
             Crie grupos para jogar seus games {'\n'}
             favoritos com seus amigos
           </Text>
-          <ButtonIcon
-            title="Entrar com Discord"
-            onPress={handleSignIn}
-          />
+          {
+            loading ? (
+              <ActivityIndicator
+                color={theme.colors.primary}
+              />
+            ) : (
+              <ButtonIcon
+                title="Entrar com Discord"
+                onPress={handleSignIn}
+              />
+            )
+          }
         </View>
       </View>
     </Background>
